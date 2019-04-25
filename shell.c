267f608e1410851fcac900c39b9ac1581876300f
mkrajnak <mkrajnak>
2019-04-25 19:23:26 +0200

    Execute single command

--- shell.c
+++ shell.c
@@ -1,13 +1,19 @@
 #define _POSIX_SOURCE
+#include <ctype.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define BUFF_LEN 513
+#define CMD_MAX 128 // Not expecting more than 128 tokens per command
 
+
+char buffer[BUFF_LEN];
+char *cmd[CMD_MAX];
 pthread_mutex_t mutex;
 pthread_cond_t await;
 int executing = 0;
@@ -33,10 +39,50 @@ void SIGCHLD_handler(){
 }
 
 
+void cmd_init() {
+  for (size_t i = 0; i < CMD_MAX; i++) {
+    cmd[i] = (char *)malloc(CMD_MAX);
+    if (&cmd[i] == NULL) {
+      err(-1, "malloc");
+    }
+    memset(cmd[i], 0, CMD_MAX);
+  }
+}
+
+
+void cmd_deinit() {
+  for (size_t i = 0; i < CMD_MAX; i++) {
+    free(cmd[i]);
+  }
+}
+
+
+void tokenize() {
+  int tokens = 0;
+  for (size_t i = 0; i < BUFF_LEN; i++) {
+    if (buffer[i] == '\0') {
+      break;
+    } else if (isspace(buffer[i])) {
+      continue;
+    } else {
+      int start = i;
+      while (!isspace(buffer[i]) && buffer[i]!='\0' && i < BUFF_LEN) {
+        ++i;
+      }
+      strncpy(cmd[tokens], &buffer[start], i-start);
+      printf("T %2d: %s\n", tokens, cmd[tokens]);
+      printf("%ld\n", strlen(cmd[tokens]) );
+      ++tokens;
+    }
+  }
+  cmd[tokens] = NULL;  // ! Required by execvp function
+}
+
+
 void *exec_thread(){
   while (running) {
     pthread_mutex_lock(&mutex);
-    while(!executing){
+    while (!executing) {  // Wait for input thread to get a cmd
       pthread_cond_wait(&await, &mutex);
     }
     if (!running) { // exit cmd has been executed
@@ -44,9 +90,26 @@ void *exec_thread(){
       pthread_mutex_unlock(&mutex);
       return NULL;
     }
+    cmd_init();
+    tokenize();
+    pid_t pid;
+    if ((pid = fork()) < 0) {
+      err(pid, "fork");
+    }
+
+    if (pid > 0) {  //parent
+      waitpid(pid, NULL, 0);
+    }
+
+    if (pid == 0) { //child
+  		int ex = execvp(cmd[0], cmd);
+  		err(ex, "execvp");
+	   }
+
     executing = 0;
     pthread_cond_broadcast(&await);
     pthread_mutex_unlock(&mutex);
+    cmd_deinit();
   }
   return NULL;
 }
@@ -54,7 +117,6 @@ void *exec_thread(){
 
 void *input_thread(){
   int cmd_len = 0;
-  char buffer[BUFF_LEN];
 
   while (running) {
     memset(&buffer, 0, BUFF_LEN);
