ea114beeea262d8c11132d9f5f3fd33564f008c7
mkrajnak <mkrajnak>
2019-04-25 15:50:01 +0200

    Thread synchronization

--- shell.c
+++ shell.c
@@ -3,9 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
+#include <unistd.h>
 
-pthread_mutex_t buff;
+#define BUFF_LEN 513
+
+pthread_mutex_t mutex;
 pthread_cond_t await;
+int executing = 0;
+int running = 1;
 
 
 void err(int code, char* err){
@@ -28,13 +34,59 @@ void SIGCHLD_handler(){
 
 
 void *exec_thread(){
-  printf("Running exec thread\n");
+  while (running) {
+    pthread_mutex_lock(&mutex);
+    while(!executing){
+      pthread_cond_wait(&await, &mutex);
+    }
+    if (!running) { // exit cmd has been executed
+      pthread_cond_broadcast(&await);
+      pthread_mutex_unlock(&mutex);
+      return NULL;
+    }
+    executing = 0;
+    pthread_cond_broadcast(&await);
+    pthread_mutex_unlock(&mutex);
+  }
   return NULL;
 }
 
 
 void *input_thread(){
-  printf("Running input thread\n");
+  int cmd_len = 0;
+  char buffer[BUFF_LEN];
+
+  while (running) {
+    memset(&buffer, 0, BUFF_LEN);
+    printf("$ ");
+    fflush(stdout);
+    cmd_len = read(0, &buffer, BUFF_LEN);
+
+    if (cmd_len == 513) {   // Invalid input
+      fprintf(stderr, "Eror ! Input way too long\n");
+      while(getchar() == '\n');
+    } else if (strncmp(buffer, "exit\n", strlen("exit\n")) == 0) {
+      pthread_mutex_lock(&mutex);
+      executing = 1;                  // Unleash the exec thread
+      running = 0;
+      pthread_cond_broadcast(&await);
+      pthread_mutex_unlock(&mutex);
+    } else if (strncmp(buffer, "\n", strlen("\n")) == 0){ // Just pressed enter
+      continue;
+    } else {  // Valid cmd, proceed to parsing in another thread
+      pthread_mutex_lock(&mutex);
+      executing = 1;                  // Unleash the exec thread
+      pthread_cond_broadcast(&await);
+      pthread_mutex_unlock(&mutex);
+      // Wait for cmd execution
+      pthread_mutex_lock(&mutex);
+      while(executing){
+        pthread_cond_wait(&await, &mutex);
+  		}
+      pthread_cond_broadcast(&await);
+      pthread_mutex_unlock(&mutex);
+    }
+  }
   return NULL;
 }
 
@@ -57,8 +109,8 @@ int main() {
   err(res, "sigaction SIGINT");
 
   // mutex and cond setup
-  res = pthread_mutex_init(&buff, NULL);
-  err(res, "pthread_mutex_init buff");
+  res = pthread_mutex_init(&mutex, NULL);
+  err(res, "pthread_mutex_init mutex");
   res = pthread_cond_init(&await, NULL);
   err(res, "pthread_cond_init await");
 
@@ -76,10 +128,10 @@ int main() {
   err(res, "pthread_join execute");
 
   // Cleanup
-  res = pthread_mutex_destroy(&buff);
-  err(res, "pthread_mutex_init buff");
   res = pthread_cond_destroy(&await);
-  err(res, "pthread_cond_init await");
+  err(res, "pthread_cond_destroy await");
+  res = pthread_mutex_destroy(&mutex);
+  err(res, "pthread_mutex_destroy mutex");
 
   return 0;
 }
